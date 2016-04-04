6b3a935c1e83585e33661ceb578604c2c0acaee8
Prateek Srivastava <prateeks@thoughtworks.com>
2016-04-04 11:31:15 +0530

    Adds test for the matrix multiplication

--- test/com/srt/la4j/MatrixTest.java
+++ test/com/srt/la4j/MatrixTest.java
@@ -19,6 +19,7 @@
 package com.srt.la4j;
 
 import com.srt.la4j.exceptions.AdditionCompatibleException;
+import com.srt.la4j.exceptions.MultiplicationCompatibleException;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -68,5 +69,43 @@ public class MatrixTest {
         Matrix matrix1 = new Matrix(array1);
 
         matrix.add(matrix1);
+    }
+
+    @Test
+    public void multiplicationOfMatrixToANumberShouldMultiplyEachElementWithThatNumber() throws Exception {
+        double[][] array = {{1, 2}, {1, 2}};
+        Matrix matrix = new Matrix(array);
+
+        Matrix result = matrix.multiply(2);
+
+        assertEquals(2d,result.get(0,0));
+        assertEquals(4d,result.get(0,1));
+        assertEquals(2d,result.get(1,0));
+        assertEquals(4d,result.get(1,1));
+    }
+
+    @Test
+    public void multiplicationOfMatrixShouldFollowTheMatrixMultiplicationRule() throws Exception {
+        double[][] array = {{1, 2}, {1, 2}};
+        double[][] array1 = {{1, 2}, {1, 2}};
+        Matrix matrix = new Matrix(array);
+        Matrix matrix1 = new Matrix(array1);
+
+        Matrix result = matrix.multiply(matrix1);
+
+        assertEquals(3d,result.get(0,0));
+        assertEquals(6d,result.get(0,1));
+        assertEquals(3d,result.get(1,0));
+        assertEquals(6d,result.get(1,1));
+    }
+
+    @Test(expected = MultiplicationCompatibleException.class)
+    public void multiplicationShouldThrowMultiplicationCompatibileExceptionInMatrixAreNotCompatible() throws Exception {
+        double[][] array = {{1,2},{1,2}};
+        double[][] array1 = {{1,2},{1,2},{1,2}};
+        Matrix matrix = new Matrix(array);
+        Matrix matrix1 = new Matrix(array1);
+
+        Matrix result = matrix.multiply(matrix1);
     }
 }
